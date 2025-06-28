baa619215f619764741d9dffc7867bdac80041c0
Divya sri Tadepalli <divyahanutadepalli@gmail.com>
2025-06-28 19:57:00 +0530

    Update image_stego_code-checkpoint.py

--- image_stego_code-checkpoint.py
+++ image_stego_code-checkpoint.py
@@ -1,58 +1,109 @@
-import cv2
-import hashlib
-import os
-d = {chr(i): i for i in range(255)}
-c = {i: chr(i) for i in range(255)}
+from PIL import Image
 
-# Reading the image
-x = cv2.imread(r"C:\Users\divya\Downloads\cover_image.jpg")
-i, j, k = x.shape
-print(i, j, k)
+def text_to_bin(text):
+    bin_str=""
+    for char in text:
+        ascii_value=ord(char)
+        bin_char=format(ascii_value,'08b')
+        bin_str+=bin_char
+    return bin_str
+    
+def bin_to_text(binary):
+    text=""
+    for i in range(0,len(binary),8):
+        byte=binary[i:i+8]
+        decimal_value=int(byte,2)
+        character=chr(decimal_value)
+        text+=character
+    return text
+    
+    
+def encoding(img_path,msg,key,output_path):
+    img=Image.open(img_path)
+    if img.mode!='RGB':
+        img=img.convert('RGB')
+    
+    pixels=img.load()   #loading the pixels of an image 
+    
+    main_msg=key+":"+msg+"||END||"  #combining secret key & message
+    
+    bin_msg=text_to_bin(main_msg)  #coverting message to binary
+    
+    index=0   #index to show position in bin_msg
+    
+    #to iterate over each pixel of the image
+    for y in range(img.height):      
+        for x in range(img.width):
+            r,g,b=pixels[x,y]
+            #red channel
+            if index<len(bin_msg):
+                bit=int(bin_msg[index])
+                #setting LSB of r to 0 using bitwise AND operation & the LSB of r to the value of bit using bitwise OR
+                r=(r&~1)|bit 
+                index+=1
+                
+            #green channel
+            if index<len(bin_msg):
+                bit=int(bin_msg[index])
+                g=(g&~1)|bit 
+                index+=1
+                
+            #blue channel
+            if index<len(bin_msg):
+                bit=int(bin_msg[index])
+                b=(b&~1)|bit 
+                index+=1
+                
+            pixels[x,y]=(r,g,b)   #updating pixels with new rgb values
+            
+    img.save(output_path)
+    print("Message encoded and saved at",output_path)
+    
+    
+def decoding(img_path,key):
+    img=Image.open(img_path)
+    pixels=img.load()   #loading the pixels of an image
+    bin_data=""
+     
+    #extracting LSB bits from each channel
+    for y in range(img.height):
+        for x in range(img.width):
+            r,g,b=pixels[x,y]
+            r1=str(r&1)  #extracting LSB of red channel
+            g1=str(g&1)  #extracting LSB of red channel
+            b1=str(b&1)  #extracting LSB of red channel
+            bin_data+=r1+g1+b1
+             
+    out_msg=bin_to_text(bin_data)
+    if "||END||" in out_msg:
+        extracted=out_msg.split("||END||")[0]
+        if ":" not in extracted:
+            return "Message format corruted."
+        decoded_key,secret_msg=extracted.split(":",1)
 
-# Input security key and text to hide
-key = input("\nEnter Security Key: ")
-text = input("\nEnter text to hide:")
-
-# Initialize variables
-k1 = 0
-n, m, z = 0, 0, 0
-l = len(text)
-
-# Encode text into the image
-for i in range(l):
-    x[n, m, z] = d[text[i]] ^ d[key[k1]]
-    z = (z + 1) % 3
-    if z == 0:
-        m = (m + 1) % j
-        if m == 0:
-            n = (n + 1) % i
-    k1 = (k1 + 1) % len(key)
-
-# Save the encrypted image
-cv2.imwrite("encrypted_img.jpg", x)
-os.startfile("encrypted_img.jpg")
-print("Data encrypted successfully.")
-
-# Initialize variables for decoding
-k1 = 0
-n, m, z = 0, 0, 0
-
-# Prompt to unhide text
-ch = int(input("\nEnter 0 To Unhide The Text: "))
-if ch == 0:
-    key_input = input("\nEnter Secret key To Unhide The Text: ")
-    decrypt = ""
-    if key == key_input:
-        for i in range(l):
-            decrypt += c[x[n, m, z] ^ d[key[k1]]]
-            z = (z + 1) % 3
-            if z == 0:
-                m = (m + 1) % j
-                if m == 0:
-                    n = (n + 1) % i
-            k1 = (k1 + 1) % len(key)
-        print("Hidden Message is:", decrypt)
+        if decoded_key==key:
+            return (secret_msg)
+        else:
+            return "Invalid secret key!"
     else:
-        print("KEY MISMATCH!!!")
-else:
-    print("EXITING!!!")
+        return "No valid message found."
+        
+#taking input from user       
+def main():
+    mode=input("Mode(encode/decode):").strip()
+    mode=mode.lower()
+     
+    if mode=="encode":
+        img_path=input("Enter input image path in .png format:").strip()
+        msg=input("Enter secret message:").strip()
+        key=input("Enter secret key:").strip()
+        output_path=input("Enter output file name:").strip()
+        encoding(img_path,msg,key,output_path)
+         
+    elif mode=="decode":
+        img_path=input("Enter encoded image path in .png format:").strip()
+        key=input("Enter secret key:").strip()
+        print(decoding(img_path,key))
+         
+main()   
+     
